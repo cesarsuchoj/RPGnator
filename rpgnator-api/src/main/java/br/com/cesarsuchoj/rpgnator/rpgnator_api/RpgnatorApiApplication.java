4d567bf103e6130de72ef1680dc938bb09c3bde9
Cesar Fernandes <cesar.suchoj@gmail.com>
2024-06-18 11:27:33 -0300

    Criado projeto da api

---
+++ rpgnator-api/src/main/java/br/com/cesarsuchoj/rpgnator/rpgnator_api/RpgnatorApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.cesarsuchoj.rpgnator.rpgnator_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgnatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgnatorApiApplication.class, args);
+	}
+
+}
